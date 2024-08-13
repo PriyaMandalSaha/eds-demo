3ecd92eddba29fe6b1055906c2ca6669d57147db
Priya <155519579+PriyaMandalSaha@users.noreply.github.com>
2024-08-13 18:02:58 +0530

    404 page changes, style changes

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,15 +1,22 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
+import { createOptimizedPicture, fetchPlaceholders } from '../../scripts/aem.js';
 
-export default function decorate(block) {
+export default async function decorate(block) {
+  const placeholders = await fetchPlaceholders('');
+  const { clickHereForMore } = placeholders;
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
+    const link = document.createElement('a');
+    link.classList.add('click-more')
+    link.href = '/home';
+    link.innerText = clickHereForMore;
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
+    li.append(link)
     ul.append(li);
   });
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));

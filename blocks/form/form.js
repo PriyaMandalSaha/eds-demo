edbe0504a0be98867dfa2684d2a22f9bc221b0a3
Priya <155519579+PriyaMandalSaha@users.noreply.github.com>
2024-08-16 12:08:25 +0530

    update form

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -37,7 +37,7 @@ function constructPayload(form) {
 async function submitForm(form) {
   const payload = constructPayload(form);
   payload.timestamp = new Date().toJSON();
-  const resp = await fetch(`https://form.aem.page/main--eds-demo--priyamandalsaha${form.dataset.action}`, {
+  const resp = await fetch(form.dataset.action, {
     method: 'POST',
     cache: 'no-cache',
     headers: {
